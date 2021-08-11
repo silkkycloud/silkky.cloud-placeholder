896645882f4a439bb66d38f014083d1bdf2f21c5
Oscar <silkky@silkky.cloud>
2021-08-11 13:17:45 +1000

    Add Privacy, TOS, Legal, Contribute, Contact pages.

--- liquid.js
+++ liquid.js
@@ -10,6 +10,27 @@ const engine = new Liquid({
 engine.renderFile('pages/index').then(file => {
     fs.writeFile('dist/index.html', file);
 });
+// Contribute
+engine.renderFile('pages/contribute').then(file => {
+    fs.writeFile('dist/contribute.html', file);
+});
+// Contact
+engine.renderFile('pages/contact').then(file => {
+    fs.writeFile('dist/contact.html', file);
+});
+
+// Privacy
+engine.renderFile('pages/privacy').then(file => {
+    fs.writeFile('dist/privacy.html', file);
+});
+// Terms of Service
+engine.renderFile('pages/tos').then(file => {
+    fs.writeFile('dist/tos.html', file);
+});
+// Legal Notice
+engine.renderFile('pages/legal').then(file => {
+    fs.writeFile('dist/legal.html', file);
+});
 
 // 404
 engine.renderFile('pages/404').then(file => {
